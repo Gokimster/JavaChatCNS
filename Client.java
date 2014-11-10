76342c28205b79641312488b1718ca6d7378cef9
Lolirot <innavsslv@gmail.com>
2014-11-10 21:23:38 +0000

    Basic Server and Threads

--- Client.java
+++ Client.java
@@ -1,9 +1,42 @@
 import java.io.*;
 import javax.net.ssl.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 
 public class Client {
-	public static void main(String[] args) {
-	 try {
+	
+	public static void main(String[] args) {	       
+		        try
+		        {
+		            Socket sock = new Socket("localhost",9999);
+		            
+		            PrintStream pr = new PrintStream(sock.getOutputStream());
+		            
+		            System.out.print("Enter Something : ");
+		          
+		            InputStreamReader rd = new InputStreamReader(System.in);
+		            BufferedReader ed = new BufferedReader(rd);
+		            
+		            String temp = ed.readLine();
+		            
+		            pr.println(temp);
+		        
+		            BufferedReader gt = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		            
+		            String tm = gt.readLine();
+		            System.out.print(tm);
+		            
+		        }
+		        catch(Exception ex)
+		        {
+		            
+		        }
+		    }
+		
+		
+	 /*try {
 	   SSLSocketFactory f =
 	          (SSLSocketFactory)SSLSocketFactory.getDefault();
 	   int port = Integer.parseInt(args[1]);
@@ -12,9 +45,8 @@ public class Client {
 	   System.out.println("mode: " + s.getSession().getCipherSuite());
 	   OutputStream output = s.getOutputStream();
 	   output.write(args[2].getBytes());
-	 } catch (Exception e) { e.printStackTrace(); }
+	 } catch (Exception e) { e.printStackTrace(); }*/
 	}
-}
 
 /* SSL example is changed to authenticate server by using
 
--- Server.java
+++ Server.java
@@ -1,35 +1,207 @@
 import java.io.*;
-import javax.net.ssl.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
 
 public class Server {
- public static void main(String[] args) {
-   byte[] buf = new byte[1024];
-   try {
-     SSLServerSocketFactory f =
-         (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
-     int port = Integer.parseInt(args[0]);
-     SSLServerSocket s = (SSLServerSocket)f.createServerSocket(port);
-     s.setEnabledCipherSuites(s.getSupportedCipherSuites());
-     SSLSocket sock = (SSLSocket)s.accept();
-     InputStream input = sock.getInputStream();
-     int n = input.read(buf);
-     System.out.println("Message: " + new String(buf, 0, n));
-   } catch (Exception e) { e.printStackTrace(); }
- }
+
+	// userID
+	public static String uID;
+	// a list keeping the client list
+	private ArrayList<ClientThread> ct;
+	// which port to be listening at
+	private int port;
+	// should the server be listening or not
+	private boolean active;
+
+	public Server(int portNO) {
+		port = portNO;
+		ct = new ArrayList<ClientThread>();
+	}
+
+	public void initialize() {
+		try {
+			// the socket used by the server
+			ServerSocket ss = new ServerSocket(port);
+			// infinite loop to wait for connections
+			while (active) {
+
+				// format message saying we are waiting, just for debugging
+				// to be removed in later versions
+				System.out.println("Server waiting for Clients on port " + port
+						+ ".");
+
+				Socket socket = ss.accept(); // accept connection
+				// if I was asked to stop
+
+				ClientThread t = new ClientThread(socket); // make a thread for
+															// the client
+				ct.add(t); // save it in the ArrayList of the client threads
+				t.start(); // initialize the thread connectin
+			}
+			// If the server has to stop, all the threads and in/out streams
+			// have to be closed.
+			try {
+				ss.close(); // close the server socket
+				for (int i = 0; i < ct.size(); ++i) { // go thtough the whole
+														// list of clients
+					ClientThread tc = ct.get(i);
+					// try to close each of them
+					try {
+						tc.in.close();
+						tc.out.close();
+						tc.socket.close();
+					} catch (IOException ioE) {
+						// not much I can do
+					}
+				}
+				// in case something doesnt want to close
+			} catch (Exception e) {
+				System.out
+						.println("Problem with the closing of the threads and the in/out streams "
+								+ e);
+			}
+		}
+		// something went wrong
+		catch (IOException e) {
+			String msg = "Exception on new ServerSocket: " + e + "\n";
+			System.out.println(msg);
+		}
+
+	}
+	
+	private synchronized void distributeMessage(Message msg){
+		String message_text = msg.getText();
+		String message_sender = msg.getSender();
+		for(int i=ct.size(); i>0; i--){
+			ClientThread current = ct.get(i);
+			if((current.sendMessage(message_text, message_sender))==0){
+				System.out.println("The user has disconnected");
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		int portnumber = 9999;
+		
+		Server server = new Server (portnumber);
+		server.initialize();
+		/*
+		 * byte[] buf = new byte[1024]; try { SSLServerSocketFactory f =
+		 * (SSLServerSocketFactory)SSLServerSocketFactory.getDefault(); int port
+		 * = Integer.parseInt(args[0]); SSLServerSocket s =
+		 * (SSLServerSocket)f.createServerSocket(port);
+		 * s.setEnabledCipherSuites(s.getSupportedCipherSuites()); SSLSocket
+		 * sock = (SSLSocket)s.accept(); InputStream input =
+		 * sock.getInputStream(); int n = input.read(buf);
+		 * System.out.println("Message: " + new String(buf, 0, n)); } catch
+		 * (Exception e) { e.printStackTrace(); }
+		 */
+	}
+
 }
-/* FOR SOCKET FACTORY
 
- SSL example is changed to authenticate server by using
+class ClientThread extends Thread {
 
-    truststore "trust" for SSL client with store password "giants"
-    keystore "key" for SSL server with store password "broncos"
-    password "rams" for private key in keystore "key"
+	private String sender;
+	public Socket socket;
+	Message message;
+	ObjectInputStream in;
+	ObjectOutputStream out;
+	boolean isActive = true;
 
+	public ClientThread(Socket sock) {
+		socket = sock;
 
-KeyStore ks = KeyStore.getInstance("JKS");
-ks.load(new FileInputStream("key"), "broncos".toCharArray());
-KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-kmf.init(ks, "rams".toCharArray());
-SSLContext sc = SSLContext.getInstance("SSLv3");
-sc.init(kmf.getKeyManagers(), null, null);
-SSLServerSocketFactory f = sc.getServerSocketFactory(); */ 
+		// try to create output/input streams
+		try {
+			in = new ObjectInputStream(sock.getInputStream());
+			out = new ObjectOutputStream(sock.getOutputStream());
+			sender = ((Message) in.readObject()).getSender();
+		} catch (IOException e) {
+			System.out
+					.println("There was an error creating the input/output stream"
+							+ e.toString());
+		} catch (ClassNotFoundException e1) {
+			System.out.println("Problem with the sender");
+		}
+
+	}
+
+	public void active() {
+		while (isActive) {
+			try {
+				message = (Message) in.readObject();
+			} catch (IOException e) {
+				System.out
+						.println("There was a problem reading the messaage object "
+								+ e);
+			} catch (ClassNotFoundException e1) {
+				// I don't know what to do in this case tbf
+			}
+			String message_text = message.getText();
+			System.out.println(sender + ":" + message_text);
+		}
+		closeConnections();
+	}
+
+	private void closeConnections() {
+		try {
+			if (in != null)
+				in.close();
+		} catch (IOException e) {
+			System.out.println("Error closing input stream" + e);
+		}
+		try {
+			if (out != null)
+				out.close();
+		} catch (IOException e1) {
+			System.out.println("Error closing output stream" + e1);
+		}
+		try {
+			if (socket != null)
+				socket.close();
+		} catch (IOException e2) {
+			System.out.println("Error closing the socket" + e2);
+		}
+	}
+
+	public int sendMessage(String mssg, String title) {
+		if (!socket.isConnected()) {
+			return 0;
+		}
+		Message newMessage = new Message(sender, title, mssg);
+		try {
+			out.writeObject(newMessage.getText());
+		} catch (IOException e) {
+			System.out.println("error writing message to server");
+		}
+
+		return 1;
+	}
+
+}
+
+/*
+ * FOR SOCKET FACTORY
+ * 
+ * SSL example is changed to authenticate server by using
+ * 
+ * truststore "trust" for SSL client with store password "giants" keystore "key"
+ * for SSL server with store password "broncos" password "rams" for private key
+ * in keystore "key"
+ * 
+ * 
+ * KeyStore ks = KeyStore.getInstance("JKS"); ks.load(new
+ * FileInputStream("key"), "broncos".toCharArray()); KeyManagerFactory kmf =
+ * KeyManagerFactory.getInstance("SunX509"); kmf.init(ks, "rams".toCharArray());
+ * SSLContext sc = SSLContext.getInstance("SSLv3");
+ * sc.init(kmf.getKeyManagers(), null, null); SSLServerSocketFactory f =
+ * sc.getServerSocketFactory();
+ */
