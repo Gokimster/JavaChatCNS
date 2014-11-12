b4fa5e8454b513717e986fb99aa882c869564db6
Lolirot <innavsslv@gmail.com>
2014-11-12 02:30:48 +0200

    Servrer is working;

--- Server.java
+++ Server.java
@@ -9,8 +9,6 @@ import javax.net.ssl.SSLSocket;
 
 public class Server {
 
-	// userID
-	public static String uID;
 	// a list keeping the client list
 	private ArrayList<ClientThread> ct;
 	// which port to be listening at
@@ -30,68 +28,84 @@ public class Server {
 			ServerSocket ss = new ServerSocket(port);
 			// infinite loop to wait for connections
 			while (active) {
-
+				System.out.println("server active loop");
 				// format message saying we are waiting, just for debugging
 				// to be removed in later versions
 				System.out.println("Server waiting for Clients on port " + port
 						+ ".");
-
+				if(!active) break;
 				Socket socket = ss.accept(); // accept connection
 				// if I was asked to stop
-
+				System.out.println("client added to list");
 				ClientThread t = new ClientThread(socket); // make a thread for
-															// the client
+				// the client
 				ct.add(t); // save it in the ArrayList of the client threads
 				t.start(); // initialize the thread connectin
 			}
 			// If the server has to stop, all the threads and in/out streams
 			// have to be closed.
-			try {
-				ss.close(); // close the server socket
-				for (int i = 0; i < ct.size(); ++i) { // go thtough the whole
-														// list of clients
-					ClientThread tc = ct.get(i);
-					// try to close each of them
-					try {
-						tc.in.close();
-						tc.out.close();
-						tc.socket.close();
-					} catch (IOException ioE) {
-						// not much I can do
-					}
-				}
-				// in case something doesnt want to close
-			} catch (Exception e) {
-				System.out
-						.println("1 Problem with the closing of the threads and the in/out streams "
-								+ e);
-			}
+			
 		}
 		// something went wrong
 		catch (IOException e) {
 			String msg = "Exception on new ServerSocket: " + e + "\n";
 			System.out.println(msg);
 		}
+		System.out.println("reset");
 
 	}
 	
-	private synchronized void distributeMessage(Message msg){
+	private void stopServer(ServerSocket ss){
+		try {
+			System.out.println("attempt to close everything");
+			ss.close(); // close the server socket
+			for (int i = 0; i < ct.size(); ++i) { // go thtough the whole
+				// list of clients
+				ClientThread tc = ct.get(i);
+				// try to close each of them
+				try {
+					tc.in.close();
+					tc.out.close();
+					tc.socket.close();
+				} catch (IOException ioE) {
+					// not much I can do
+				}
+			}
+			// in case something doesnt want to close
+		} catch (Exception e) {
+			System.out
+					.println("1 Problem with the closing of the threads and the in/out streams "
+							+ e);
+		}
+	}
+
+	public synchronized void distributeMessage(Message msg) {
 		String message_text = msg.getText();
-		String message_author = msg.getAuthor();
-		for(int i=ct.size(); i>0; i--){
+		String author = msg.getAuthor();
+		System.out.println(author + ">>>"+message_text);
+		for (int i = ct.size()-1; i > -1; i--) {
 			ClientThread current = ct.get(i);
-			if((current.sendMessage(message_text, message_author))==0){
+			//try to send them a meessage
+			int sent = current.sendMessage(msg);
+			if (sent == 0) {
 				System.out.println("The user has disconnected");
 			}
 		}
-		
+
 	}
 
+	/*for(int i = 0; i < al.size(); ++i) {
+            ClientThread ct = al.get(i);
+		           // found it
+		         if(ct.id == id) {
+	               al.remove(i);
+	                return;*/
+	
 	public static void main(String[] args) {
-		
+
 		int portnumber = 9999;
-		
-		Server server = new Server (portnumber);
+
+		Server server = new Server(portnumber);
 		server.initialize();
 		/*
 		 * byte[] buf = new byte[1024]; try { SSLServerSocketFactory f =
@@ -106,50 +120,62 @@ public class Server {
 		 */
 	}
 
-}
+
 
 class ClientThread extends Thread {
 
-	private String author;
+	
+	// private String author;
 	public Socket socket;
-	Message message;
 	ObjectInputStream in;
 	ObjectOutputStream out;
-	boolean isActive = true;
+	
+	boolean isActive;
 
 	public ClientThread(Socket sock) {
+		isActive = true;
 		socket = sock;
 		System.out.println("New thread connected");
 		// try to create output/input streams
 		try {
+			System.out.println("Thread trying to create Object Input/Output Streams");
 			in = new ObjectInputStream(sock.getInputStream());
 			out = new ObjectOutputStream(sock.getOutputStream());
-			//author = ((Message) in.readObject()).getAuthor();
+			// author = ((Message) in.readObject()).getAuthor();
+			System.out.println("Thread IO win!!");
 		} catch (IOException e) {
 			System.out
 					.println("There was an error creating the input/output stream"
 							+ e.toString());
-		} //catch (ClassNotFoundException e1) {
-		//	System.out.println("Problem with the sender");
-		//}
+		}
+		active();// catch (ClassNotFoundException e1) {
+			// System.out.println("Problem with the sender");
+			// }
 
 	}
 
 	public void active() {
 		while (isActive) {
+			System.out.println("In Thread's active while loop");
 			try {
-				message = (Message) in.readObject();
+				System.out.println("reading message from thread");
+				Message message = (Message) in.readObject();
+				System.out.println("propagading message");
+				distributeMessage(message);
 			} catch (IOException e) {
-				System.out
-						.println("There was a problem reading the messaage object "
+				System.out.println("There was a problem reading the messaage object "
 								+ e);
 			} catch (ClassNotFoundException e1) {
 				// I don't know what to do in this case tbf
 			}
-			String message_text = message.getText();
-			System.out.println(author + ":" + message_text);
+			
+			
+			// String message_text = message.getText();
+			// System.out.println(message.getAuthor()+ ":" + message_text);
 		}
 		closeConnections();
+		
+	
 	}
 
 	private void closeConnections() {
@@ -173,22 +199,23 @@ class ClientThread extends Thread {
 		}
 	}
 
-	public int sendMessage(String mssg, String title) {
+	public int sendMessage(Message msg) {
 		if (!socket.isConnected()) {
 			return 0;
 		}
-		Message newMessage = new Message(author, title, mssg);
 		try {
-			out.writeObject(newMessage.getText());
+			System.out.println("trying to send a message");
+			out.writeObject(msg);
 		} catch (IOException e) {
-			System.out.println("error writing message to server");
+			System.out.println("error sending message"+ e);
 		}
 
 		return 1;
 	}
 
-}
 
+}
+}
 /*
  * FOR SOCKET FACTORY
  * 
