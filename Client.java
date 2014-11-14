efa057ad9fbf71b4a582ffd30d666b429b97eef1
Tamara Tanasescu <gokimster@yahoo.com>
2014-11-14 12:24:47 +0000

    Cleaned code

--- Client.java
+++ Client.java
@@ -152,27 +152,3 @@ public class Client
     }	
     }
 
-
-/*try {
-SSLSocketFactory f =
-(SSLSocketFactory)SSLSocketFactory.getDefault();
-int port = Integer.parseInt(args[1]);
-SSLSocket s = (SSLSocket)f.createSocket(args[0], port);
-s.setEnabledCipherSuites(s.getSupportedCipherSuites());
-System.out.println("mode: " + s.getSession().getCipherSuite());
-OutputStream output = s.getOutputStream();
-output.write(args[2].getBytes());
-} catch (Exception e) { e.printStackTrace(); }*/
-
-/* SSL example is changed to authenticate server by using
-truststore "trust" for SSL client with store password "giants"
-keystore "key" for SSL server with store password "broncos"
-password "rams" for private key in keystore "key"
-KeyStore ks = KeyStore.getInstance("JKS");
-ks.load(new FileInputStream("trust"), "giants".toCharArray());
-TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-tmf.init(ks);
-SSLContext sc = SSLContext.getInstance("SSLv3");
-sc.init(null, tmf.getTrustManagers(), null);
-SSLSocketFactory f = sc.getSocketFactory();
-*/
--- Server.java
+++ Server.java
@@ -41,9 +41,6 @@ public class Server {
 			
 			// infinite loop to wait for connections
 			while (active) {
-				System.out.println("server active loop");
-				// format message saying we are waiting, just for debugging
-				// to be removed in later versions
 				System.out.println("Server waiting for Clients on port " + port
 						+ ".");
 				if(active==false)
@@ -70,7 +67,6 @@ public class Server {
 						tc.out.close();
 						tc.socket.close();
 					} catch (IOException ioE) {
-						// not much I can do
 					}
 				}
 				// in case something doesnt want to close
@@ -111,22 +107,11 @@ public class Server {
 				ct.remove(toClose);
 			}
 		}
-		/*private int sendToClient(ArrayList<Message> list,ClientThread client){
-			
-			if(client.sendMessage(null, messageList)==2){
-				System.out.println("message list send successfully");
-				return 1;
-			}
-			System.out.println("Unsuccessful message send");
-			return 0;
-			
-		}*/
 		
 	public synchronized void distributeMessage(Message msg) {
 		messageList.add(msg);
 		String message_text = msg.getText();
 		String author = msg.getAuthor();
-		System.out.println(author + ">>>"+message_text);
 		if(ct.size()>0){
 		for (ClientThread c : ct){
 			System.out.println("sending message to peers");
@@ -138,13 +123,6 @@ public class Server {
 		}
 		}
 	}
-
-	/*for(int i = 0; i < al.size(); ++i) {
-            ClientThread ct = al.get(i);
-		           // found it
-		         if(ct.id == id) {
-	               al.remove(i);
-	                return;*/
 
 	public static void main(String[] args) {
 		
@@ -159,17 +137,6 @@ public class Server {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		/*
-		 * byte[] buf = new byte[1024]; try { SSLServerSocketFactory f =
-		 * (SSLServerSocketFactory)SSLServerSocketFactory.getDefault(); int port
-		 * = Integer.parseInt(args[0]); SSLServerSocket s =
-		 * (SSLServerSocket)f.createServerSocket(port);
-		 * s.setEnabledCipherSuites(s.getSupportedCipherSuites()); SSLSocket
-		 * sock = (SSLSocket)s.accept(); InputStream input =
-		 * sock.getInputStream(); int n = input.read(buf);
-		 * System.out.println("Message: " + new String(buf, 0, n)); } catch
-		 * (Exception e) { e.printStackTrace(); }
-		 */
 	}
 
 
@@ -181,9 +148,6 @@ class ClientThread extends Thread {
 	public Socket socket;
 	ObjectInputStream in;
 	ObjectOutputStream out;
-	//this is a problem cause message is saved and afterwards when it keeps looping it 
-	//wont terminate cause it has something to loop over; 
-	//dunno how to do it tho cause kinda lost and tired.
 	
 	boolean isActive;
 
@@ -196,15 +160,12 @@ class ClientThread extends Thread {
 			System.out.println("Thread trying to create Object Input/Output Streams");
 			in = new ObjectInputStream(sock.getInputStream());
 			out = new ObjectOutputStream(sock.getOutputStream());
-			// author = ((Message) in.readObject()).getAuthor();
-			System.out.println("Thread IO win!!");
+			System.out.println("Thread IO win successful");
 		} catch (IOException e) {
 			System.out.println("There was an error creating the input/output stream"
 							+ e.toString());
-		} // catch (ClassNotFoundException e1) {
-			// System.out.println("Problem with the sender");
-			// }
-		//active();
+		} 
+		
 	}
 
 	public void run() {
@@ -219,7 +180,6 @@ class ClientThread extends Thread {
 					 if(message.getText().equals("MESSAGE_LIST")){
 						 sendMessage(null,messageList);
 					 }else if(message.getText().equals("SEARCH")){
-						 System.out.println("SEARCH REQUESTE RECEIVED"+message.getAuthor()+" "+message.getTitle());
 						 
 						 for (int i = messageList.size() -1 ; i >= 0; i--)
 					    {
@@ -229,7 +189,6 @@ class ClientThread extends Thread {
 					        if ((message.getTitle().equals("")) || (m.getTitle().equals(message.getTitle ())))
 					        {
 					          searchResult.add(m);
-					          System.out.println(m.getAuthor()+"TITLE:"+m.getTitle());
 					        }
 					      }
 					      else
@@ -239,12 +198,10 @@ class ClientThread extends Thread {
 					          if (m.getTitle().equals(message.getTitle()))
 					          {
 					            searchResult.add(m);
-					            System.out.println(m.getAuthor()+"TITLE:"+m.getTitle());
 					          }
 					        }
 					      }
 					    }
-						 System.out.println("searchSIZE TO SEND OVER"+searchResult.size());
 						 sendMessage(null,searchResult);
 				 }
 					 
@@ -259,12 +216,7 @@ class ClientThread extends Thread {
 				destructor();
 			} catch (ClassNotFoundException e1) {}
 			System.out.println("propagading message");
-			System.out.println("checking for null messages");
 				
-			// String message_text = message.getText();
-			// System.out.println(message.getAuthor()+ ":" + message_text)
-			//isActive =false;
-			
 		}
 		
 	closeConnections();	
@@ -320,7 +272,6 @@ class ClientThread extends Thread {
 	}
 	}else if(messages!=null) {
 		try{
-		//System.out.println("SERVER FINAL EMSSSAGE LIST"+ messageList.size());
 	    ArrayList<Message> clonedList = new ArrayList<Message>(messages.size());
 	    System.out.println(clonedList.size());
 	    for (Message temp : messages) {
@@ -336,52 +287,3 @@ class ClientThread extends Thread {
 }
 }
 
-
-/*
- * for (int i = messages.size() -1 ; i >= 0; i--)
-    {
-      Message m = messages.get(i);
-      System.out.println(m.getAuthor()+"TITLE:"+m.getTitle());
-      if (m.getAuthor().equals(authorSearch.getText()))
-      {
-        if ((titleSearch.getText().equals("")) || (m.getTitle().equals(titleSearch.getText())))
-        {
-          if (++counter % 2 != 0)
-            midPanel.add(createMessagePanel(m, false));
-          else 
-            midPanel.add(createMessagePanel(m, true));
-        }
-      }
-      else
-      {
-        if (authorSearch.getText().equals(""))
-        {
-          if (m.getTitle().equals(titleSearch.getText()))
-          {
-            if (++counter % 2 != 0)
-              midPanel.add(createMessagePanel(m, false));
-            else 
-              midPanel.add(createMessagePanel(m, true));
-          }
-        }
-      }
-    }
- */
-
-/*
- * FOR SOCKET FACTORY
- * 
- * SSL example is changed to authenticate server by using
- * 
- * truststore "trust" for SSL client with store password "giants" keystore "key"
- * for SSL server with store password "broncos" password "rams" for private key
- * in keystore "key"
- * 
- * 
- * KeyStore ks = KeyStore.getInstance("JKS"); ks.load(new
- * FileInputStream("key"), "broncos".toCharArray()); KeyManagerFactory kmf =
- * KeyManagerFactory.getInstance("SunX509"); kmf.init(ks, "rams".toCharArray());
- * SSLContext sc = SSLContext.getInstance("SSLv3");
- * sc.init(kmf.getKeyManagers(), null, null); SSLServerSocketFactory f =
- * sc.getServerSocketFactory();
- */
